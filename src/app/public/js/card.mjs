52dd0d8dc9e204e2a833fb9795ef69b2c96842b2
jaketbailey <jake@jaketbailey.co.uk>
2021-12-23 22:01:39 +0000

    basic basket UI set up

--- src/app/public/js/card.mjs
+++ src/app/public/js/card.mjs
@@ -1,15 +1,17 @@
-const createCard = () => {
+const createCard = (text, i) => {
   const card = document.createElement('div');
   card.className = 'card';
-  card.textContent = 'This is a test';
+  card.id = `card-${i}`;
+  card.textContent = text;
   return card;
 };
 
 const load = () => {
   const page = document.getElementById('page');
-  for (let i = 0; i < 48; i++) {
+  const text = 'Card Element';
+  for (let i = 0; i < 5; i++) {
     page.appendChild(document.createElement('div'));
-    page.lastChild.appendChild(createCard());
+    page.lastChild.appendChild(createCard(text, i));
   }
 };
 
