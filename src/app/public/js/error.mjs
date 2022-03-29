41660a7a0d48950abbe522d95db0e4dda2ca23b2
jaketbailey <jake@jaketbailey.co.uk>
2022-03-29 10:58:52 +0100

    small update to error handler

--- src/app/public/js/error.mjs
+++ src/app/public/js/error.mjs
@@ -1,28 +1,15 @@
 export default async function (error) {
-  console.log('test');
-  console.log(error);
   const data = {
     message: error.message,
     stack: error.stack,
   };
   console.log(JSON.stringify(data));
-  const response = await fetch('/block/api/error/', {
+  await fetch('/block/api/error/', {
     headers: {
       'Content-Type': 'application/json',
     },
     method: 'POST',
     body: JSON.stringify(data),
   });
-  return response.json();
+  throw error;
 }
-//   window.onerror = async (msg, url, lineNo, columnNo, error) => {
-//     const errorObj = {
-//       message: msg,
-//       url: url,
-//       lineNo: lineNo,
-//       columnNo: columnNo,
-//       error: error,
-//     };
-//     console.log(errorObj);
-//   };
-// }
