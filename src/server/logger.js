3da4f270e934616d91ed5d42943e63adba32c240
jaketbailey <jake@jaketbailey.co.uk>
2022-03-29 11:18:41 +0100

    date added to log

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,5 +1,17 @@
 import colors from 'colors';
 
+function getDate() {
+  const dateObj = new Date();
+  const day = dateObj.getDate();
+  const month = dateObj.getMonth() + 1;
+  const year = dateObj.getFullYear();
+  const hour = dateObj.getHours();
+  const minute = dateObj.getMinutes();
+  const second = dateObj.getSeconds();
+  const full = `${day}-${month}-${year} ${hour}:${minute}:${second}`;
+  return full;
+}
+
 colors.setTheme({
   Info: 'green',
   Warn: 'yellow',
@@ -10,25 +22,25 @@ colors.setTheme({
 });
 
 export const Info = (...args) => {
-  console.log(`${'[INFO]'.Info} ${args}`);
+  console.log(`[${getDate()}] ${'[INFO]'.Info} ${args}`);
 };
 
 export const Warn = (...args) => {
-  console.log(`${'[WARN]'.Warn} ${args}`);
+  console.log(`[${getDate()}] ${'[WARN]'.Warn} ${args}`);
 };
 
 export const Error = (...args) => {
-  console.log(`${'[ERROR]'.Error} ${args}`);
+  console.log(`[${getDate()}] ${'[ERROR]'.Error} ${args}`);
 };
 
 export const Database = (...args) => {
-  console.log(`${'[DATABASE]'.Database} ${args}`);
+  console.log(`[${getDate()}] ${'[DATABASE]'.Database} ${args}`);
 };
 
 export const Express = (...args) => {
   if (args[1]) {
-    console.log(`${'[EXPRESS]'.Express} ${`[${args[1]}]`.Request} ${args[0]}`);
+    console.log(`[${getDate()}] ${'[EXPRESS]'.Express} ${`[${args[1]}]`.Request} ${args[0]}`);
   } else {
-    console.log(`${'[EXPRESS]'.Express} ${args[0]}`);
+    console.log(`[${getDate()}] ${'[EXPRESS]'.Express} ${args[0]}`);
   }
 };
