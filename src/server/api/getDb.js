8cd5fb36df48f6bf9e27e4f3d9e2ed3d3f870d53
jaketbailey <jake@jaketbailey.co.uk>
2022-03-30 19:07:44 +0100

    minor updates

--- src/server/api/getDb.js
+++ src/server/api/getDb.js
@@ -3,111 +3,163 @@ const Query = require('../db/getQueries.js');
 
 const getAllProducts = (req, res) => {
   Logger.Express('/block/api/shop/all', 'GET');
-  const results = Query.getAllProducts();
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getAllProducts()
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const getProductById = (req, res) => {
   const id = parseInt(req.params.id, 10);
   Logger.Express(`/block/api/shop/item/${id}`, 'GET');
-  const results = Query.getProductById(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getProductById(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const getUser = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/get-user/${id}`, 'GET');
-  const results = Query.getUser(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getUser(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const checkExists = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/check-exists/${id}`, 'GET');
-  const results = Query.checkExists(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.checkExists(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const getBasketItems = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/get-basket-items/${id}`, 'GET');
   Logger.Database(`Getting basket items for user with ID: ${id}`);
-  const results = Query.getBasketItems(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getBasketItems(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const getStock = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/get-stock/${id}`, 'GET');
-  const results = Query.getStock(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getStock(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const getTotalCost = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/get-total-cost/${id}`, 'GET');
-  const results = Query.getTotalCost(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getTotalCost(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const getUserName = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/get-user-name/${id}`, 'GET');
-  const results = Query.getUserName(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getUserName(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const getBasketId = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/get-basket/${id}`, 'GET');
-  const results = Query.getBasketId(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getBasketId(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 
 const getProductByFilter = (req, res) => {
   const { filter } = req.params;
   Logger.Express(`/block/api/type-filters/${filter}`, 'GET');
-  const results = Query.getProductByFilter(filter);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.getProductByFilter(filter)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const checkOrderDetail = (req, res) => {
   const id = req.params.id;
   Logger.Express(`/block/api/check-order-detail/${id}`, 'GET');
-  const results = Query.checkOrderDetail(id);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.checkOrderDetail(id)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 const searchProduct = (req, res) => {
   const search = req.params.search;
   Logger.Express(`/block/api/search-product/${search}`, 'GET');
-  const results = Query.searchProduct(search);
-  results.then(data => {
-    res.status(200).json(data);
-  });
+  Query.searchProduct(search)
+    .then(data => {
+      res.status(200).json(data);
+    })
+    .catch(err => {
+      Logger.Error(err);
+      throw err;
+    });
 };
 
 module.exports = {
