b3300b699b634ef827e2538cc8d5ee85d34aa8e8
jaketbailey <jake@jaketbailey.co.uk>
2022-03-30 22:18:29 +0100

    new tests and remove comments

--- src/__tests/db.test.js
+++ src/__tests/db.test.js
@@ -11,9 +11,24 @@ describe('getDb', () => {
 
   test('Add User', async () => {
     await insertQuery.createUser('testuser2-testuser2', 'Test User', 'test2@gmail.com');
+    await insertQuery.createBasket('testuser2-testuser2', 'test2@gmail.com');
     await getQuery.getUserName('testuser2-testuser2')
       .then((data) => {
         expect(data).toBeDefined();
+      });
+    await getQuery.getBasketId('testuser2-testuser2')
+      .then((data) => {
+        expect(data).toBeDefined();
+      });
+  });
+  test('Delete User', async () => {
+    await getQuery.getBasketId('testuser2-testuser2')
+      .then(async (data) => {
+        await deleteQuery.deleteUser('testuser2-testuser2', data[0].id);
+      });
+    await getQuery.getUserName('testuser2-testuser2')
+      .then((data) => {
+        expect(data).toEqual([]);
       });
   });
 
@@ -34,42 +49,3 @@ describe('getDb', () => {
       });
   });
 });
-
-// const DoMaths = (a, b) => a + b;
-// const DoQuadratic = (a, b, c) => a * a + b * b + c * c;
-
-// const StateObject = {};
-
-// test('Logic System', () => {
-//   expect(DoMaths(1, 2)).toBe(3);
-//   expect(DoQuadratic(1, 2, 3)).toBe(14);
-
-//   StateObject.x = 1;
-//   StateObject.y = 2;
-//   expect(StateObject.x).toBe(1);
-// });
-
-// function NewUser(username, email, password) {
-//   if (!email.includes('@')) {
-//       throw new Error('Invalid email');
-//   }
-
-//   Database.insert({
-//       username,
-//       email,
-//       password,
-//   });
-// }
-
-// test('User System', () {
-//   NewUser('test', 'test@gmail.com', 'test');
-//   expect(Database.get('test')).toBeDefined();
-
-//   try {
-//       NewUser('test1', 'asdasdasd', 'test');
-//   } catch (e) {
-//       expect(e.message).toBe('Invalid email');
-//   }
-//   expect(Database.get('test1')).toNotBeDefined();
-// })
-
