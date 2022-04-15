4cb4e99cfa13198a70f42d61660684d0a3fb5195
jaketbailey <jake@jaketbailey.co.uk>
2022-04-15 10:01:08 +0100

    more product tests

---
+++ src/__tests/orders.test.js
@@ -0,0 +1,7 @@
+/**
+ * @file products.test.js
+ * @author UP2002753
+ * @description Contains all the tests for the user's database interactions. This includes adding to basketm new user, updating user, deleting etc.
+ */
+
+/* eslint-disable */ 
--- src/__tests/products.test.js
+++ src/__tests/products.test.js
@@ -1,7 +1,7 @@
 /**
  * @file products.test.js
  * @author UP2002753
- * @description Contains all the tests for the user's database interactions. This includes adding to basketm new user, updating user, deleting etc.
+ * @description Contains all the tests for the product database interactions
  */
 
 /* eslint-disable */ 
